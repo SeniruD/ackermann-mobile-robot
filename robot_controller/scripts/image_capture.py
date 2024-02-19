6049be757f6a3acec9ac91364b59548bb54999d4
senirud <senirudissanayake@gmail.com>
2024-02-19 15:31:18 +0530

    Obtain normalized depth images

--- robot_controller/scripts/image_capture.py
+++ robot_controller/scripts/image_capture.py
@@ -6,35 +6,84 @@ from std_msgs.msg import String
 from sensor_msgs.msg import Image
 from cv_bridge import CvBridge
 import cv2
+import numpy as np
+import torch
 
-def image_callback( data): 
-    rospy.loginfo("Image recieved")
+def rgb_image_callback( data): 
+    rospy.loginfo("RGB image recieved")
     bridge = CvBridge()
     try:
-        # Convert the ROS Image message to an OpenCV image
-        cv_image = bridge.imgmsg_to_cv2(data, "bgr8")
-        cv2.imshow("Image window", cv_image)
+        cv_rgb_image = bridge.imgmsg_to_cv2(data, "bgr8")
+        cv2.imshow("rgb image window", cv_rgb_image)
             
-        (h, w, channels) = cv_image.shape
+        (h, w, channels) = cv_rgb_image.shape
         y = (w-h)//2
-        rospy.loginfo("Image size: %d x %d", h, w)
-        rospy.loginfo(cv_image)
-        leftImage = cv_image[:, y:y+h]
-        leftImage = cv2.resize(leftImage,(224,224))
-        rospy.loginfo(leftImage.shape)
+        rospy.loginfo("rgb image size: %d x %d", w, h)
+        rospy.loginfo(cv_rgb_image)
+        rgb_image = cv_rgb_image[:, y:y+h]
+        rgb_image = cv2.resize(rgb_image,(224,224))
+        rospy.loginfo(rgb_image.shape)
         
-        cv2.imshow("cropped window", leftImage)
+        cv2.imshow("cropped rgb window", rgb_image)
+        cv2.waitKey(0) 
+        cv2.destropAllWindows()
+
+
+    except Exception as e:
+        rospy.logerr("Error processing image: %s", str(e))
+
+    rgb_image_sub.unregister()
+
+def depth_image_callback(ros_image): 
+    rospy.loginfo("Depth image recieved")
+    bridge = CvBridge()
+    try:
+        cv_depth_image = bridge.imgmsg_to_cv2(ros_image, "32FC1")
+
+        # Replace inf, -inf, and NaN values with 0
+        cv_depth_image[np.isinf(cv_depth_image) | np.isnan(cv_depth_image)] = 0
+        # Normalize the depth image values
+        # normalized_depth_image = cv2.normalize(cv_depth_image, None, 0, 255, cv2.NORM_MINMAX, dtype=cv2.CV_8U)
+
+        # Normalize the depth image values
+        min_val = np.min(cv_depth_image)
+        max_val = np.max(cv_depth_image)
+        if max_val != min_val:
+            normalized_depth_image = (cv_depth_image - min_val) / (max_val - min_val)
+        else:
+            normalized_depth_image = np.zeros_like(cv_depth_image)
+        
+        # Display the normalized depth image
+        cv2.imshow("Normalized Depth Image", normalized_depth_image)
+        
+        # Display the normalized depth image
+        # cv2.imshow("Normalized Depth Image", normalized_depth_image)
+        # rospy.loginfo("depth image min: %f, max: %f", normalized_depth_image.min(), normalized_depth_image.max())
+     
+        # resivve the depth image to 256x256
+        (h, w) = normalized_depth_image.shape
+        y = (w-h)//2
+        depth_image = normalized_depth_image[:, y:y+h]
+        depth_image = cv2.resize(depth_image,(256,256))
+        depth_image = torch.tensor([depth_image], device='cuda:0')
+        rospy.loginfo(depth_image.shape)
+        
+        cv2.imshow("cropped depth window", depth_image)
+        rospy.loginfo(depth_image)
         cv2.waitKey(0) 
         cv2.destropAllWindows()
     except Exception as e:
         rospy.logerr("Error processing image: %s", str(e))
-    image_sub.unregister()
+    
+    depth_image_sub.unregister()
 
 
 
 def take_image():
-    global image_sub
-    image_sub = rospy.Subscriber('/zed2/zed_node/rgb/image_rect_color', Image, image_callback)
+    global rgb_image_sub, depth_image_sub
+    # rgb_image_sub = rospy.Subscriber('/zed2/zed_node/rgb/image_rect_color', Image, rgb_image_callback)
+    depth_image_sub = rospy.Subscriber('/zed2/zed_node/depth/depth_registered', Image, depth_image_callback)
+
     rospy.init_node('image_taker')
     rospy.spin()
 
